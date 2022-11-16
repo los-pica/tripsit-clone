74391159a4c808251f2013c71ea5913d88ed680a
Wenceslao Posse Silva <wencho22@gmail.com>
2022-11-16 12:19:37 +0100

    feat: create contact page (#12)
    * implemented contact.jsx
    
    * implemented react-router-dom

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,16 +1,23 @@
 //  import { useState } from "react";
 import Header from "../header/Header";
-import Content from "../content/Content";
-import Footer from "../footer/Footer";
+//import Content from "../content/Content";
+//import Footer from "../footer/Footer";
+import Contact from "../contact/Contact";
 import "./app.scss";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app">
+    <>
       <Header />
-      <Content />
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      {/*<div className="app">
+        <Content />
+        <Footer />
+  </div> */}
+    </>
   );
 };
 
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Links from "./Links";
+import "./Contact.scss";
+export default function Contact() {
+  return (
+    <div className="contact-container">
+      <h1>Contact us</h1>
+      <p>
+        We’re always here to listen to feedback or any questions users have. To
+        help direct your message to the right person, please choose the most
+        relevant option below.
+      </p>
+      <Links />
+    </div>
+  );
+}
