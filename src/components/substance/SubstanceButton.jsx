25d90f0c95dbd72815532fc1c39b87f4def80b84
Wenceslao Posse Silva <wencho22@gmail.com>
2022-11-17 18:22:08 +0100

    lifted state from substanceButton to substanceCombination

--- src/components/substance/SubstanceButton.jsx
+++ src/components/substance/SubstanceButton.jsx
@@ -1,24 +1,13 @@
 import React, { useState } from "react";
 import "./substance.scss";
-/*
-const substances = [
-  { id: 1, name: "Cannabis", combinations: "" },
-  { id: 2, name: "LSD", combinations: "" },
-  { id: 3, name: "Cocaine", combinations: "" },
-];
-*/
-function SubstanceButton(props) {
-  const [active, setActive] = useState(false);
-  console.log(active);
 
+function SubstanceButton({ substance, onClick }) {
   return (
     <button
-      onClick={() => {
-        setActive(!active);
-      }}
-      style={{ opacity: active ? 1.0 : 0.5 }}
+      onClick={() => onClick(substance.id)}
+      style={{ opacity: substance.active ? 1.0 : 0.5 }}
     >
-      {props.name}
+      {substance.name}
     </button>
   );
 }
--- src/components/substance/SubstanceCombination.jsx
+++ src/components/substance/SubstanceCombination.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import SubstanceButton from "./SubstanceButton";
 
-const substances = [
-  { id: 1, name: "Cannabis", combinations: "" },
-  { id: 2, name: "LSD", combinations: "" },
-  { id: 3, name: "Cocaine", combinations: "" },
-];
-function SubstanceCombination() {
+const SubstanceCombination = () => {
+  const [substance, setSubstance] = useState([
+    { id: 1, name: "Cannabis", active: false, combinations: "" },
+    { id: 2, name: "LSD", active: false, combinations: "" },
+    { id: 3, name: "Cocaine", active: false, combinations: "" },
+  ]);
+  const onSubstanceClick = (id) => {
+    const substanceCopy = [...substance];
+    const actualSubstance = substanceCopy.find(
+      (substance) => substance.id === id
+    );
+    actualSubstance.active = !actualSubstance.active;
+    setSubstance(substanceCopy);
+    console.log(id, substance);
+  };
   return (
     <div>
       <h3>Choose the substances:</h3>
       <div className="substance-list">
-        {substances.map((substance) => (
-          <SubstanceButton key={substance.id} name={substance.name} />
+        {substance.map((substance) => (
+          <SubstanceButton
+            key={substance.id}
+            substance={substance}
+            onClick={onSubstanceClick}
+          />
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default SubstanceCombination;
