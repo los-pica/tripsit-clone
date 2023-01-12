14057e15a6a70971cac17c55207943a6d0cd617e
Wenceslao Posse Silva <wencho22@gmail.com>
2023-01-12 17:39:46 +0100

    feat: substance component (#14)
    * created substance button
    
    * added substance combination component
    
    * substance.scss added
    
    * lifted state from substanceButton to substanceCombination
    
    * added css to components
    
    * feat: implemented review comments

---
+++ src/components/substance/SubstanceButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./substance-button.scss";
+
+function SubstanceButton({ substance, onClick }) {
+  return (
+    <button
+      onClick={() => onClick(substance.id)}
+      style={{ opacity: substance.active ? 1.0 : 0.5 }}
+    >
+      {substance.name}
+    </button>
+  );
+}
+
+export default SubstanceButton;
